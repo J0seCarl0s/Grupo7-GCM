5e86236730b210b92666b92027801e039bfe05b5
fsalinac <flaviosalinascari@gmail.com>
2021-07-02 19:26:05 -0500

    Delete Desarrollo/SGR/Sprint 1/Scripts directory

--- Desarrollo/SGR/Sprint 1/Scripts/existencias_cantidad.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-	insumos.idinsumo as Id,
-    insumos.nombre_insumo as Insumo, 
-    SUM(cantidad) as Total 
-FROM historial_almacen 
-INNER JOIN insumos on historial_almacen.idinsumo=insumos.idinsumo
-GROUP BY historial_almacen.idinsumo;
